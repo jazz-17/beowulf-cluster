bf0566eb101e635e7e1267d061ce68ec1cab63b4
jazz17 <kevinquispe117@gmail.com>
2025-05-05 05:19:14 -0500

    add matrix example

--- src/mpi/main.c
+++ src/mpi/main.c
@@ -1,11 +1,13 @@
-#include <mpi.h>    // Main MPI header
-#include <stdio.h>  // For printf
+#include <mpi.h>
+#include <stdio.h>
 #include <unistd.h> // For gethostname
+#include <string.h>
+
+#define MAX_MESSAGE_SIZE 100
 
 int main(int argc, char **argv)
 {
     // Initialize the MPI environment
-    // MPI_Init takes pointers to argc and argv to handle any MPI-specific command-line arguments
     MPI_Init(&argc, &argv);
 
     // Get the total number of processes in the communicator MPI_COMM_WORLD
@@ -13,16 +15,32 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); // MPI_COMM_WORLD is the default group of all processes
 
     // Get the rank (unique ID, 0 to world_size-1) of the current process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     // Get the hostname of the node where this process is running
     char hostname[256];
     gethostname(hostname, sizeof(hostname));
 
-    // Print a message including hostname, rank, and total size
-    printf("Hello from node %s, rank %d out of %d processors\n",
-           hostname, world_rank, world_size);
+    char greeting[MAX_MESSAGE_SIZE];
+
+    if (my_rank != 0)
+    {
+        sprintf(greeting, "Greetings from process %d of %d !", my_rank, world_size);
+        MPI_Send(greeting, strlen(greeting) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        printf("Greetings from process %d of %d !", my_rank, world_size);
+        for (int i = 0; i < world_size; i++)
+        {
+            MPI_Recv(greeting, MAX_MESSAGE_SIZE, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%s\n", greeting);
+        }
+    }
+    // // Print a message including hostname, rank, and total size
+    // printf("Hello from node %s, rank %d out of %d processors\n",
+    //        hostname, my_rank, world_size);
 
     // Finalize the MPI environment. No MPI calls should be made after this.
     MPI_Finalize();
